149032ffcae0a7b5d9e56963f9922f8a94110410
Rody Huancas <rodyhuancas.04@gmail.com>
2023-08-27 18:50:54 -0500

    Add: authentication and reset password

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -1,3 +1,4 @@
+import generarId from "../helpers/generarId.js";
 import generarJWT from "../helpers/generarJWT.js";
 import Veterinario from "../models/Veterinario.js";
 
@@ -5,7 +6,6 @@ import Veterinario from "../models/Veterinario.js";
 // res -> respuesta del servidor
 
 const registrar = async (req, res) => {
-
     const { nombre, email, password } = req.body;
 
     // Prevenir usuarios duplicados
@@ -13,7 +13,7 @@ const registrar = async (req, res) => {
     const existeUsuario = await Veterinario.findOne({ email });
     if (existeUsuario) {
         const error = new Error("El usuario ya está registrado.");
-        return res.status(400).json({ msg: error.message })
+        return res.status(400).json({ msg: error.message });
     }
 
     try {
@@ -25,13 +25,17 @@ const registrar = async (req, res) => {
         res.send(veterinarioGuardado);
     } catch (error) {
         console.log(error);
-
     }
-}
+};
 
 const perfil = (req, res) => {
-    res.send({ mdg: "mostrar perfil" });
-}
+    // estraer veterinario que está guuardado en la sesion
+    const { veterinario } = req;
+
+    res.send({
+        perfil: veterinario,
+    });
+};
 
 const confirmar = async (req, res) => {
     // obterner el token de la url
@@ -40,8 +44,8 @@ const confirmar = async (req, res) => {
     const usuarioConfirmar = await Veterinario.findOne({ token });
     // si no existe el usuario con ese token
     if (!usuarioConfirmar) {
-        const error = new Error("Token no válido.")
-        return res.status(404).json({ msg: error.message })
+        const error = new Error("Token no válido.");
+        return res.status(404).json({ msg: error.message });
     }
 
     try {
@@ -56,7 +60,7 @@ const confirmar = async (req, res) => {
     } catch (error) {
         console.log(`Error: ${error.message}`);
     }
-}
+};
 
 const autenticar = async (req, res) => {
     const { email, password } = req.body;
@@ -66,26 +70,92 @@ const autenticar = async (req, res) => {
 
     // si no existe el usuario
     if (!usuario) {
-        const error = new Error("El usuario no existe.")
-        return res.status(404).json({ msg: error.message })
+        const error = new Error("El usuario no existe.");
+        return res.status(404).json({ msg: error.message });
     }
 
     // Comprobar si el usuario está confirmado
     if (!usuario.confirmado) {
-        const error = new Error("Tu cuenta no ha sido confirmada.")
-        return res.status(404).json({ msg: error.message })
+        const error = new Error("Tu cuenta no ha sido confirmada.");
+        return res.status(404).json({ msg: error.message });
     }
 
     // Autenticar al usuario, el comprobarPassword está definido en el model
     if (await usuario.comprobarPassword(password)) {
         // autenticar
-        res.json({ token: generarJWT(usuario._id) })
+        res.json({ token: generarJWT(usuario._id) });
     } else {
-        const error = new Error("La contraseña es incorrecta.")
-        return res.status(404).json({ msg: error.message })
+        const error = new Error("La contraseña es incorrecta.");
+        return res.status(404).json({ msg: error.message });
     }
-}
+};
+
+const olvidePassword = async (req, res) => {
+    const { email } = req.body;
+    // verificar si el email que enviamos existe en algun usuario registrado
+    const existeVeterinario = await Veterinario.findOne({ email });
+    // si no existe
+    if (!existeVeterinario) {
+        const error = new Error("El usuario no existe.");
+        return res.status(400).json({ msg: error.message });
+    }
+
+    try {
+        // generar un nuevo token
+        existeVeterinario.token = generarId();
+        // guardar usuario con un nuevo token
+        await existeVeterinario.save();
+        res.json({ msg: "Hemos enviado un email con las instrucciones." });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const comprobarToken = async (req, res) => {
+    const { token } = req.params;
+    // verificar por token que usuario lo tiene
+    const tokenValido = await Veterinario.findOne({ token });
+
+    if (tokenValido) {
+        // El token es valido, el usuario existe
+        res.json({ msg: "Token válido, el usuario existe." })
+    } else {
+        const error = new Error("Token no válido.");
+        return res.status(404).json({ msg: error.message });
+    }
+};
+const nuevoPassword = async (req, res) => {
+    const { token } = req.params;
+    const { password } = req.body;
+
+    // buscar usuario con el token
+    const veterinario = await Veterinario.findOne({ token });
+    if (!veterinario) {
+        const error = new Error("Hubo un error.");
+        return res.status(400).json({ msg: error.message });
+    }
+
+    try {
+        // pasar el token a null
+        veterinario.token = null;
+        // actualizar la contraseña
+        veterinario.password = password;
+
+        // guardar cambios
+        await veterinario.save()
+
+        res.json({ msg: "Contraseña actualizada correctamente." });
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 export {
-    registrar, perfil, confirmar, autenticar
-}
+    registrar,
+    perfil,
+    confirmar,
+    autenticar,
+    olvidePassword,
+    comprobarToken,
+    nuevoPassword,
+};
--- backend/routes/veterinarioRoutes.js
+++ backend/routes/veterinarioRoutes.js
@@ -1,11 +1,21 @@
 import express from 'express'
-import { autenticar, confirmar, perfil, registrar } from '../controllers/veterinarioController.js';
+import { autenticar, comprobarToken, confirmar, nuevoPassword, olvidePassword, perfil, registrar } from '../controllers/veterinarioController.js';
+import checkAuth from '../middleware/autrhMiddleware.js';
 
 const router = express.Router();
 
+// AREA PÚBLICA
 router.post("/", registrar);
-router.get("/perfil", perfil);
 router.get("/confirmar/:token", confirmar);
 router.post("/login", autenticar);
+router.post("/olvide-password", olvidePassword);
+// router.get("/olvide-password/:token", comprobarToken)
+// router.post("/olvide-password/:token", nuevoPassword)
+router.route("/olvide-password/:token").get(comprobarToken).post(nuevoPassword);
+
+
+// AREA PRIVADA
+// cuando vayamos a la ruta, primero entrará a la funcuón de checkAuth y luego a perfil
+router.get("/perfil", checkAuth, perfil);
 
 export default router;
